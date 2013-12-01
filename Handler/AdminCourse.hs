a5dc6d16f4de5a5dd9bdd6b73ca445dcaa688db3
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-02 02:18:19 +0400

    AdminCourse and AdminNewCourse empty handlers

---
+++ Handler/AdminCourse.hs
@@ -0,0 +1,6 @@
+module Handler.AdminCourse where
+
+import Import
+
+getAdminCourseR :: CourseId -> Handler Html
+getAdminCourseR = error "Not yet implemented: getAdminCourseR"
---
+++ Handler/AdminNewCourse.hs
@@ -0,0 +1,9 @@
+module Handler.AdminNewCourse where
+
+import Import
+
+getAdminNewCourseR :: Handler Html
+getAdminNewCourseR = error "Not yet implemented: getAdminNewCourseR"
+
+postAdminNewCourseR :: Handler Html
+postAdminNewCourseR = error "Not yet implemented: postAdminNewCourseR"
