a84cb2b5a255821bff01e768e8be3786ed6b45fb
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-01 18:12:25 +0400

    removed initial text from homepage

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -4,18 +4,8 @@ module Handler.Home where
 import Import
 import Yesod.Auth
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe (FileInfo, Text)
-        handlerName = "getHomeR" :: Text
     ma <- maybeAuth
     let maybeLogin = userIdent . entityVal <$> ma
     defaultLayout $ do
@@ -24,20 +14,3 @@ getHomeR = do
         $(widgetFile "header")
         $(widgetFile "homepage")
 
-postHomeR :: Handler Html
-postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
-
-    defaultLayout $ do
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
-
-sampleForm :: Form (FileInfo, Text)
-sampleForm = renderDivs $ (,)
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField "What's on the file?" Nothing
