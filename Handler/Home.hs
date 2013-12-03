40170e3dae54a34d3edf3a6eba78b709095de63c
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-04 03:22:47 +0400

    enhanced home page

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,9 +2,12 @@
 module Handler.Home where
 
 import Import
+import Yesod.Auth
+import Data.Maybe
 
 getHomeR :: Handler Html
 getHomeR = do
+  loggedIn <- isJust <$> maybeAuthId
   defaultLayout $ do
     setTitle "trass - The Rigorous Assignment Submission System"
     $(widgetFile "homepage")
