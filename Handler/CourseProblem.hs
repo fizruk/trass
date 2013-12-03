d19676a351d370475effa9a1574e9e7fb7263184
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-04 00:09:39 +0400

    added problem page

--- Handler/CourseProblem.hs
+++ Handler/CourseProblem.hs
@@ -1,6 +1,15 @@
 module Handler.CourseProblem where
 
 import Import
+import CourseRepository
 
 getCourseProblemR :: CourseId -> FilePath -> Handler Html
-getCourseProblemR = error "Not yet implemented: getCourseProblemR"
+getCourseProblemR courseId prob = do
+  c <- runDB $ get404 courseId
+  let repo = courseRepo c
+  p <- readCourseRepositoryProblem repo prob
+  case p of
+    Nothing -> notFound
+    Just problem -> do
+      defaultLayout $ do
+        $(widgetFile "course-problem")
