804f1c8a069e0f700479fd056412b4c8e85c3579
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-04 05:03:57 +0400

    submit a solution

--- Handler/CourseProblem.hs
+++ Handler/CourseProblem.hs
@@ -13,3 +13,15 @@ getCourseProblemR courseId prob = do
     Just problem -> do
       defaultLayout $ do
         $(widgetFile "course-problem")
+
+postCourseProblemR :: CourseId -> FilePath -> Handler Html
+postCourseProblemR courseId prob = do
+  c <- runDB $ get404 courseId
+  let repo = courseRepo c
+  p <- readCourseRepositoryProblem repo prob
+  case p of
+    Nothing -> notFound
+    Just problem -> do
+      file <- lookupFile "solutionFile"
+      -- TODO: add to submit branch in course repo
+      redirect HomeR
