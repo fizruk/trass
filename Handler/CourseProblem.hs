bc842ad6729a6d31c6a750addc0cde330e80e7d2
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-06 00:09:48 +0400

    added CodeMirror editor to problem page

--- Handler/CourseProblem.hs
+++ Handler/CourseProblem.hs
@@ -1,7 +1,9 @@
 module Handler.CourseProblem where
 
 import Import
+import Data.List
 import CourseRepository
+import CodeMirror
 
 getCourseProblemR :: CourseId -> FilePath -> Handler Html
 getCourseProblemR courseId prob = do
@@ -12,6 +14,12 @@ getCourseProblemR courseId prob = do
     Nothing -> notFound
     Just problem -> do
       defaultLayout $ do
+        addStylesheet $ StaticR codemirror_lib_codemirror_css
+        addScript     $ StaticR codemirror_lib_codemirror_js
+        let languages = [CM_Haskell]
+        case lookup (head languages) codeMirrorModes of
+          Just mode_js -> addScript mode_js
+          Nothing      -> return ()
         $(widgetFile "course-problem")
 
 postCourseProblemR :: CourseId -> FilePath -> Handler Html
