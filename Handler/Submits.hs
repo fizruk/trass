88ef266b078f5bcd76e7a8b003a2fcd8b1a1262a
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-14 02:13:22 +0400

    stupid submits page

--- Handler/Submits.hs
+++ Handler/Submits.hs
@@ -1,8 +1,12 @@
 module Handler.Submits where
 
 import Import
+import Yesod.Auth
 
 getSubmitsR :: Handler Html
 getSubmitsR = do
+  userId <- requireAuthId
+  ss <- runDB $ selectList [SubmissionUser ==. userId] [Desc SubmissionId]
+  let submits = map entityVal ss
   defaultLayout $ do
     $(widgetFile "submits")
