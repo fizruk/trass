cc602067e78fbc9c6a4ca4ec8aff906c802fa324
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-14 17:42:50 +0400

    added problem handler

--- Application.hs
+++ Application.hs
@@ -28,6 +28,7 @@ import Handler.TeacherNewCourse
 import Handler.Course
 import Handler.Courses
 import Handler.CourseSection
+import Handler.CourseProblem
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/CourseProblem.hs
@@ -0,0 +1,6 @@
+module Handler.CourseProblem where
+
+import Import
+
+getCourseProblemR :: CourseId -> [Text] -> Handler Html
+getCourseProblemR = error "Not yet implemented: getCourseProblemR"
