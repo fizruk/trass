7d0e93ec71733d1cf45f01f41a95739e9291a74f
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-15 00:53:10 +0400

    display deadline and CodeMirror editor

--- Handler/CourseProblem.hs
+++ Handler/CourseProblem.hs
@@ -1,7 +1,8 @@
 module Handler.CourseProblem where
 
-import Import
+import Import hiding (parseTime)
 import CourseRepository
+import qualified Data.Yaml.Config as Y
 
 getCourseProblemR :: CourseId -> [Text] -> Handler Html
 getCourseProblemR courseId ppath = do
@@ -11,5 +12,10 @@ getCourseProblemR courseId ppath = do
   mtitle    <- liftIO $ cpTitle cp
   mdesc     <- liftIO $ cpDescription cp
   msnippet  <- liftIO $ cpSnippet cp
+  mc <- liftIO $ cpConfig cp
+  let mdeadline = mc >>= Y.lookup "deadline" :: Maybe Text
+      webEditor = maybe True (Y.lookupDefault "web-editor" True) mc
   defaultLayout $ do
+    addStylesheet $ StaticR codemirror_lib_codemirror_css
+    addScript     $ StaticR codemirror_lib_codemirror_js
     $(widgetFile "problem")
