9d900bd89c9f70f6f3387e8a6a863f68898bcfa8
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-15 01:18:06 +0400

    automatically load CodeMirror mode

--- Handler/CourseProblem.hs
+++ Handler/CourseProblem.hs
@@ -1,7 +1,9 @@
 module Handler.CourseProblem where
 
 import Import hiding (parseTime)
+import CodeMirror
 import CourseRepository
+import Control.Monad
 import qualified Data.Yaml.Config as Y
 
 getCourseProblemR :: CourseId -> [Text] -> Handler Html
@@ -13,9 +15,13 @@ getCourseProblemR courseId ppath = do
   mdesc     <- liftIO $ cpDescription cp
   msnippet  <- liftIO $ cpSnippet cp
   mc <- liftIO $ cpConfig cp
-  let mdeadline = mc >>= Y.lookup "deadline" :: Maybe Text
-      webEditor = maybe True (Y.lookupDefault "web-editor" True) mc
+  let mdeadline  = mc >>= Y.lookup "deadline" :: Maybe Text
+      mlanguages = mc >>= Y.lookup "languages"
+      webEditor  = maybe True (Y.lookupDefault "web-editor" True) mc
   defaultLayout $ do
     addStylesheet $ StaticR codemirror_lib_codemirror_css
     addScript     $ StaticR codemirror_lib_codemirror_js
+    case (mlanguages >>= msum . map codeMirrorMode) of
+      Just mode_js -> addScript mode_js
+      Nothing      -> return ()
     $(widgetFile "problem")
