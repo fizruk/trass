2400b504fbbec06cae6681a94b5e7da24872aa24
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-14 15:37:09 +0400

    basic courses page

--- Handler/Courses.hs
+++ Handler/Courses.hs
@@ -1,6 +1,19 @@
 module Handler.Courses where
 
 import Import
+import CourseRepository
+import Control.Monad
 
 getCoursesR :: Handler Html
-getCoursesR = error "Not yet implemented: getCoursesR"
+getCoursesR = do
+  cs <- runDB $ selectList [] [Asc CourseId]
+  courses <- forM cs $ \(Entity courseId course) -> do
+    let name = courseName course
+    cr <- courseContents name
+    title <- liftIO $ csTitle cr
+    desc  <- liftIO $ csDescription cr
+    sections <- liftIO $ csSubsections cr
+    problems <- liftIO $ csProblems cr
+    return (title, desc, sections, problems)
+  defaultLayout $ do
+    $(widgetFile "courses")
