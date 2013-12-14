e8e1a9ed97752a0703fd998bcb6c000488cf3b95
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-14 18:05:25 +0400

    basic problem page

--- Handler/CourseProblem.hs
+++ Handler/CourseProblem.hs
@@ -1,6 +1,15 @@
 module Handler.CourseProblem where
 
 import Import
+import CourseRepository
 
 getCourseProblemR :: CourseId -> [Text] -> Handler Html
-getCourseProblemR = error "Not yet implemented: getCourseProblemR"
+getCourseProblemR courseId ppath = do
+  course <- runDB $ get404 courseId
+  let name = courseName course
+  cp <- problemContents404 name ppath
+  mtitle    <- liftIO $ cpTitle cp
+  mdesc     <- liftIO $ cpDescription cp
+  msnippet  <- liftIO $ cpSnippet cp
+  defaultLayout $ do
+    $(widgetFile "problem")
