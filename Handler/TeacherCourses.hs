d729a231752f2f1c92582825c75a92e8f48b8fd3
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-14 06:35:44 +0400

    basic info for teacher courses page

--- Handler/TeacherCourses.hs
+++ Handler/TeacherCourses.hs
@@ -4,6 +4,7 @@ import Import
 
 getTeacherCoursesR :: Handler Html
 getTeacherCoursesR = do
+  courses <- runDB $ selectList [] [Asc CourseId]
   defaultLayout $ do
     setTitle "Teacher's corner: courses"
     $(widgetFile "teacher-courses")
