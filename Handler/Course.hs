0237f4ff15dab63fd7a2cc9e503477e010a51abe
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-12-14 16:04:46 +0400

    basic course page

--- Handler/Course.hs
+++ Handler/Course.hs
@@ -1,6 +1,16 @@
 module Handler.Course where
 
 import Import
+import CourseRepository
 
 getCourseR :: CourseId -> Handler Html
-getCourseR = error "Not yet implemented: getCourseR"
+getCourseR courseId = do
+  course <- runDB $ get404 courseId
+  let name = courseName course
+  cr <- courseContents name
+  mtitle <- liftIO $ csTitle cr
+  mdesc  <- liftIO $ csDescription cr
+  sections <- liftIO $ csSubsections cr
+  problems <- liftIO $ csProblems cr
+  defaultLayout $ do
+    $(widgetFile "section")
